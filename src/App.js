c75a08ea95e4a24a0f4c086928ecc586d14f504e
Ashish Augustine <ashish.augustine123@gmail.com>
2023-02-02 19:16:51 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function App() {
   return (
     <main>
       <section className="search">
-        <p>SEARCH ANY IMAGE..</p>
+        <h2>SEARCH ANY IMAGES</h2>
         <form className="search-form">
           <input
             type="text"
